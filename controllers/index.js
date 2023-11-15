5172d89096c79f647f7022f449880cfd3f3435c4
Issac.Le <issac.le@avepoint.com>
2023-11-15 13:20:12 +0700

    fix-conflict

--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,5 @@ import UserController from "./UserController.js";
 import AreaController from "./AreaController.js";
 import MapController from "./MapController.js";
 import BannerController from "./BannerController.js";
+
 export { UserController, AreaController, MapController, BannerController };

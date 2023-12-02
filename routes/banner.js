16b2e1fe9e3608fe89cd0d418c7e1280aa005a83
Issac.Le <issac.le@avepoint.com>
2023-12-02 22:16:11 +0700

    change admin route and permission

--- routes/banner.js
+++ routes/banner.js
@@ -3,9 +3,9 @@ import { BannerController } from "../controllers/index.js";
 
 const router = express.Router();
 
-router.get("/all", BannerController.getAllList);
+router.post("/all", BannerController.getAllList);
 router.post("/create", BannerController.addNewBanner);
-router.post("/update", BannerController.updateBaner);
+router.patch("/update", BannerController.updateBaner);
 router.get("/banner/:id", BannerController.deleteBanner);
 
 export default router;

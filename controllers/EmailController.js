6df9d0077f71555a204a27785ad37cac5fe13ae6
Issac.Le <issac.le@avepoint.com>
2024-01-11 20:08:07 +0700

    change email

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -1,9 +1,38 @@
-import { sendRegistionCodeEmail } from "../services/EmailCustom.js";
+import { Campaign } from "../models/index.js";
+import { sendEmailByType } from "../services/EmailCustom.js";
 
 const sendEmail = async (req, res) => {
   try {
-    const { type, id, emailList } = req.body;
-    sendRegistionCodeEmail();
+    const { type, campaignID, emailList } = req.body;
+    const campaignData = await Campaign.findById(campaignID).exec();
+    let data;
+    if (type === "cancel") {
+      data = {
+        userName,
+        campaignName: campaignData.title,
+        reason,
+        toEmail: emailList,
+      };
+    } else if (type === "reject") {
+      data = {
+        userName,
+        campaignName: campaignData.title,
+        reason,
+        fileUrl,
+        endDate: campaignData.endDate,
+        toEmail: emailList,
+      };
+    } else {
+      data = {
+        userName,
+        campaignName: campaignData.title,
+        reason,
+        fileUrl,
+        endDate: campaignData.endDate,
+        toEmail: emailList,
+      };
+    }
+    sendEmailByType({ ...data, toEmail: emailList }, type);
     res.status(HttpStatusCode.OK).json({
       message: Message.success,
       result: data,
@@ -14,3 +43,4 @@ const sendEmail = async (req, res) => {
     });
   }
 };
+export default { sendEmail };
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { HomeController } from "../controllers/index.js";
+import { EmailController, HomeController } from "../controllers/index.js";
+import Role from "../utils/Role.js";
 
 const router = express.Router();
 
 router.get("/user", HomeController.getCountForHome);
 router.get("/admin", HomeController.getCountForAdmin);
-
+router.post("/email", EmailController.sendEmail);
 export default router;
